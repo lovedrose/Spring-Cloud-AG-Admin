a6824b11851ece3929b018f941ff8bf16609688c
薛骞 <xueqian@sunaw.net>
2018-02-01 14:57:24 +0800

    重构groupId和artifactId

--- config-center/src/main/java/com/lovehins/config/center/ConfigServerBootstrap.java
+++ config-center/src/main/java/com/lovehins/config/center/ConfigServerBootstrap.java
@@ -1,11 +1,12 @@
-package com.github.wxiaoqi.security.config;
+package com.lovehins.config.center;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 /**
- * Created by ace on 2017/7/29.
+ * 配置中心
+ * Created by lovehins on 2017/7/29.
  */
 @EnableAutoConfiguration
 @EnableEurekaClient
--- register-center/src/main/java/com/lovehins/register/center/CenterBootstrap.java
+++ register-center/src/main/java/com/lovehins/register/center/CenterBootstrap.java
@@ -1,17 +1,16 @@
-package com.github.wxiaoqi.security.center;
+package com.lovehins.register.center;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
- * ${DESCRIPTION}
+ * 启动一个服务注册中心提供给其他应用进行对话
  *
- * @author wanghaobin
+ * @author lovehins
  * @create 2017-05-25 12:44
  */
-@EnableEurekaServer  //启动一个服务注册中心提供给其他应用进行对话
+@EnableEurekaServer
 @SpringBootApplication
 public class CenterBootstrap {
     public static void main(String[] args) {
